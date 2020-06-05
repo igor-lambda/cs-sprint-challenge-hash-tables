b4644ce8ae8527cacd4bf18dd3ba64e6c476a374
igor <embiggenerd@gmail.com>
2020-06-05 15:46:57 -0400

    ex1 passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+def order_pair(pair):
+    if pair[0] > pair[1]:
+        return pair
+    else:
+        return pair[::-1]
 
-    return None
+def get_indices_of_item_weights(weights, length, limit):
+    tup = None
+    cache = {}
+    for i in range(length):
+        diff = limit - weights[i]
+        if diff in cache:
+            tup = order_pair((cache[diff], i))
+            break
+        cache[weights[i]] = i
+    return tup
